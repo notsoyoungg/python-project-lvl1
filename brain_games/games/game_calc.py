f9a3cb21f8e78491257b6fd9a44147d5ce9cbbf6
notsoyoungg <p.sergey1997@mail.ru>
2022-05-18 17:42:06 +0600

    add game_calc.py and game_even.py

---
+++ brain_games/games/game_calc.py
@@ -0,0 +1,18 @@
+from random import choice
+from random import randint
+
+game_question = 'What is the result of the expression?'
+
+def game():
+  action = choice(('*', '+', '-'))
+  num1 = randint(1, 10)
+  num2 = randint(1, 10)
+  question = (f'{num1} {action} {num2}')
+  if action == '*':
+    correct_answer = num1 * num2
+  if action == '+':
+    correct_answer = num1 + num2
+  if action == '-':
+    correct_answer = num1 - num2
+  cortege = (question, correct_answer)
+  return(cortege)
---
+++ brain_games/games/game_even.py
@@ -0,0 +1,14 @@
+import prompt
+from random import randint
+
+game_question = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def game():
+  question = randint(1, 100)
+  if question % 2 == 0:
+    correct_answer = 'yes'
+  if question % 2 != 0:
+    correct_answer = 'no'
+  cortege = (question, correct_answer)
+  return(cortege)
