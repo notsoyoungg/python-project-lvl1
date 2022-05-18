5e0cd869b02d46721b6ad5a68b5bb4e54fa15145
notsoyoungg <p.sergey1997@mail.ru>
2022-05-18 22:16:57 +0600

    fixes

--- brain_games/main_logic.py
+++ brain_games/main_logic.py
@@ -1,21 +1,21 @@
 import prompt
 
 
-def main(g, question):
+def main(func, variable):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(game_question)
+    print(variable)
     i = 0
     while i < 3:
-      get = game()
-      print(f'Question: {get[0]}')
+      anwer_or_question = func()
+      print(f'Question: {anwer_or_question[0]}')
       answer = prompt.string('Your answer: ')
-      if answer.lower() == str(get[1]).lower():
+      if answer.lower() == str(anwer_or_question[1]).lower():
         print('Correct!')
         i += 1
         if i == 3:
           print(f'Congratulations, {name}!')
       else:
-        print(f"'{answer}' is wrong answer ;( Correct answer was '{get[1]}'.")
+        print(f"'{answer}' is wrong answer ;( Correct answer was '{anwer_or_question[1]}'.")
         print(f"Let's try again, {name}")
         break
