84dd84c5eedb9d5ca46a372bfb94fdde84d1962f
notsoyoungg <p.sergey1997@mail.ru>
2022-05-18 17:40:49 +0600

    fix brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from ..main_logic import main
+from brain_games.games.game_even import game
+from brain_games.games.game_even import game_question
+
+def script():
+    main()
+
+
+if __name__ == '__main__':
+    scripts()
