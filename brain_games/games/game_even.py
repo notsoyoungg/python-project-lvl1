00b8caa5a7ac87da863590eef7724e065de00684
notsoyoungg <p.sergey1997@mail.ru>
2022-05-19 22:28:29 +0600

    linter's fixes

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -1,14 +1,13 @@
-import prompt
 from random import randint
 
 game_question = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def game():
-  question = randint(1, 100)
-  if question % 2 == 0:
-    correct_answer = 'yes'
-  if question % 2 != 0:
-    correct_answer = 'no'
-  cortege = (question, correct_answer)
-  return(cortege)
+    question = randint(1, 100)
+    if question % 2 == 0:
+        correct_answer = 'yes'
+    if question % 2 != 0:
+        correct_answer = 'no'
+    cortege = (question, correct_answer)
+    return(cortege)
--- brain_games/main_logic.py
+++ brain_games/main_logic.py
@@ -8,15 +8,15 @@ def logic(func, variable):
     print(variable)
     i = 0
     while i < 3:
-      anwer_or_question = func()
-      print(f'Question: {anwer_or_question[0]}')
-      answer = prompt.string('Your answer: ')
-      if answer.lower() == str(anwer_or_question[1]).lower():
-        print('Correct!')
-        i += 1
+        data = func()
+        print(f'Question: {data[0]}')
+        inp = prompt.string('Your answer: ')
+        if inp.lower() == str(data[1]).lower():
+            print('Correct!')
+            i += 1
         if i == 3:
-          print(f'Congratulations, {name}!')
-      else:
-        print(f"'{answer}' is wrong answer ;( Correct answer was '{anwer_or_question[1]}'.")
-        print(f"Let's try again, {name}")
-        break
+            print(f'Congratulations, {name}!')
+        else:
+            print(f"'{inp}' is wrong answer ;( Correct answer was '{data[1]}'.")
+            print(f"Let's try again, {name}")
+            break
