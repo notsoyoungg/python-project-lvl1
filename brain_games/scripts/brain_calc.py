97d37c3fb58fb87270a859bf00e771ddccfc6e63
notsoyoungg <p.sergey1997@mail.ru>
2022-05-19 22:09:14 +0600

    add brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from ..main_logic import logic
+from ..games.game_calc import game
+from ..games.game_calc import game_question
+
+
+def main():
+    logic(game, game_question)
+
+
+if __name__ == '__main__':
+    main()
