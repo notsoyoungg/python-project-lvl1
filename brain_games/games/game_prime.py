d4944fb9e2946a9740dc98fafd3f1adcb9d5033a
notsoyoungg <p.sergey1997@mail.ru>
2022-05-23 17:26:59 +0600

    fix game_prime.py

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -5,7 +5,10 @@ game_question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def game():
     number = randint(1, 100)
-    if number % 2 != 0:
+    divisor = 2
+    while number % divisor != 0:
+        divisor += 1
+    if divisor == number:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
